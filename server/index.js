c43b7236e94d57fe155840b8971c1051d40b5dfe
Lukcy-Leaf <yx1030@live.com>
2018-03-22 00:28:36 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const Koa=require('koa');
+const app=new Koa();
+const {normal}=require('./tpl');
+
+app.use(async (ctx,next)=>{
+    ctx.body=normal;
+});
+
+app.listen(8080,()=>{
+    console.log('server is running...');
+});
